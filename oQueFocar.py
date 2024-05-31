e78c2b40fda16442ec02ea4fdefb9f0f53c62458
dansilx <danilosilveira@msn.com>
2024-05-31 00:31:03 -0300

    salvar

---
+++ oQueFocar.py
@@ -0,0 +1,20 @@
+'''
+Array
+LinkedList
+Queue
+Stack
+Binary Tree
+Hashmap
+Graph
+
+BIG O
+
+Identificar tipos de problemas
+- DFS
+- BFS
+- Sliding Window
+- Backtracking
+- DP (Fibonacci)
+- Hashmap
+- TwoPointer
+'''
