2b319bf461291567c9264d08495adcf177475e41
dansilx <danilosilveira@msn.com>
2024-06-18 13:47:57 -0300

    enviando recursão caixa

---
+++ recursion_grokk/search_key.py
@@ -0,0 +1,18 @@
+'''
+def search_key(main_box):
+    pile = main_box.create_a_pile_4_search()
+    while pile is not empty:
+        box = pile.get_box()
+        for item in box:
+            if item.is_a_box():
+                pile.append(item)
+            elif item.is_a_key():
+                print("found the key!")
+                '''
+
+def search_box(box):
+    for item in box:
+        if item.is_a_box():
+            search_box(item)
+        elif item.is_a_key():
+            print("found the key")
